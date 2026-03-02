1ee4448b26026769687b58e12e9ed7c30d967a82
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-02 15:47:52 +0200

    update code

--- OpenHash.java
+++ OpenHash.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 // Ramatheledza ADZISANI 4484298
 public class OpenHash {
 
@@ -12,4 +14,31 @@ public class OpenHash {
         }
 
     }
+
+    // let's stsrt the open hash
+    static int N = 20;
+    static LinkedList <String> [] table = new LinkedList[N];
+
+    public static void main(String[] args){
+        // we need to initialise the linked list
+        for(int i = 1 ; i < N ; i++){
+            table[i] = new LinkedList<>();
+
+        }
+        insert ( "1 " , "ben");
+    }
+
+    // create am insert method
+    static void insert(String key , String value){
+        int index = key % N;
+        table[index].add(key);
+
+    }
+
+    // create a method for String lookup
+
+
+
+
+
 }
