e6b67cc6234c5766d13588609827bed4bf46ea41
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-02 14:54:49 +0200

    lets start hashing

--- A14Practical.java
+++ A14Practical.java
@@ -1,6 +1,40 @@
 // Ramatheledza Adzisani 4484298
 // practical 4 - hash tables
+// copy the code for the previous prac and edit it
 
+import java.text.DecimalFormat;
+import java.util.Random;
 
 public class A14Practical {
+    public class timeMethods{
+
+    }
+    public static int N = 30;
+    public static void main(String [] args) {
+        DecimalFormat twoD = new DecimalFormat("0.00");
+        DecimalFormat fourD = new DecimalFormat(" 0.0000");
+        DecimalFormat fiveD = new DecimalFormat("0.00000");
+
+        long start, finish;
+        double runTime = 0;
+        double runTime2 = 0;
+        double time;
+        double totalTime = 0.0;
+        int n = N;
+        int repetition, repetitions = 30;
+
+        int[] array = new int[32654];
+        Random rand = new Random();
+
+        for (int i = 0; i < 32654; i++) {
+            array[i] = i + 1;
+        }
+
+        runTime = 0;
+
+
+    }
+    
+
+
 }
